70f8f711cec2a55317ce79f721e25c5c639e979e
Your Name <you@example.com>
2020-03-14 11:38:36 -0700

    first form

--- corona/calc/admin.py
+++ corona/calc/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Post
+from .models import CalcRisk
 
-admin.site.register(Post)
+admin.site.register(CalcRisk)
--- corona/calc/views.py
+++ corona/calc/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import CalcRiskForm
+from .models import CalcRisk
 
-# Create your views here.
+def calc_risk(request):
+    calcform = CalcRiskForm()
+    return render(request, 'calc/calc_risk.html', {'calcform': calcform})
