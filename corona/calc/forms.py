de5c6d32f7bdca9fb06cae0d796b2367b3bbe23d
Your Name <you@example.com>
2020-03-15 18:57:31 -0700

    adding calculator

---
+++ corona/calc/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from .models import CalcRisk
+
+class CalcRiskForm(forms.ModelForm):
+    genderChoices = [('M','Male'),('F','Female')]
+    gender=forms.CharField(label='Gender', widget=forms.RadioSelect(choices=genderChoices))
+    age = forms.CharField(label = "Age", max_length = 10)
+
+    conditionChoices = [('1','Cardiovascular Disease'),
+                        ('2','Diabetes'),
+                        ('3', 'Chronic respiratory disease'),
+                        ('4', 'Hypertension'),
+                        ('5', 'Cancer'),
+                        ('6', 'None'),]
+    
+    condition=forms.CharField(label='Comorbities', required=False,
+                              widget=forms.CheckboxSelectMultiple(choices=conditionChoices))
+
+    class Meta:
+        model = CalcRisk
+        fields = ()
--- corona/calc/views.py
+++ corona/calc/views.py
@@ -3,5 +3,16 @@ from .forms import CalcRiskForm
 from .models import CalcRisk
 
 def calc_risk(request):
+    if request.method == "POST":
+        calcform = CalcRiskForm(request.POST)
+        if calcform.is_valid():
+            gender = calcform.data['gender']
+            age = calcform.data['age']
+            condition = calcform['condition'].value()
+            return render(request, 'calc/calc_risk.html',
+                          {'calcform': calcform,
+                           'riskfactor': '20%'})
+    
     calcform = CalcRiskForm()
     return render(request, 'calc/calc_risk.html', {'calcform': calcform})
+        
