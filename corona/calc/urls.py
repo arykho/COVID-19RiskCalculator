1b4098d12039200a3665fadbdcc6800fedea18fe
Your Name <you@example.com>
2020-03-17 10:30:25 -0700

    fix python 2.7 compatibility

--- corona/calc/urls.py
+++ corona/calc/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.calc_risk, name='calc_risk'),
+    url('', views.calc_risk, name='calc_risk'),
 ]
